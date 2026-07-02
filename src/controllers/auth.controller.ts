01a0fb13af7a6b6fa70dfd99b444c24bbc353633
Babita Katwal <katwalbabita7@gmail.com>
2026-07-02 12:45:20 +0545

    Implement password hashing for user registration

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-
+import {hash, compare} from "../utils/bcrypt.utils";
 import User from "../models/user.model";
 // * register(create user)
 export const register = async (req:Request,res:Response,next:NextFunction) => {
@@ -24,8 +24,17 @@ export const register = async (req:Request,res:Response,next:NextFunction) => {
             throw error;
         }
 
-        const user = await User.create({full_name, email, password, phone});
+        // const user = await User.create({full_name, email, password, phone});
+        const user = new User({full_name, email, phone});
 
+        // * password hash
+        const hashPass = await hash(password);
+        user.password = hashPass;
+
+        // * handle image upload
+
+        // * save 
+        await user.save();
         // * send success response
         res.status(201).json({
             message:"Account created",
@@ -39,6 +48,62 @@ export const register = async (req:Request,res:Response,next:NextFunction) => {
     
 }
 // * login
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+
+    if(!email){
+            const error: any = new Error("email is required");
+            error.statusCode = 400;
+            error.status= "fail";
+            throw error;
+        }
+        if(!password){
+            const error: any = new Error("password is required");
+            error.statusCode = 400;
+            error.status= "fail";
+            throw error;
+        }
+
+        // const { email, password } = req.body;
+
+//* Find user by email
+const user = await User.findOne({ email }).select("+password");
+
+if (!user) {
+    const error: any = new Error("Invalid Credential");
+    error.statusCode = 400;
+    error.status = "fail";
+    throw error;
+}
+// * compare password
+const isPassMatched = compare(password,user.password);
+
+if (!isPassMatched) {
+    const error: any = new Error("Invalid Credential");
+    error.statusCode = 400;
+    error.status = "fail";
+    throw error;
+}
+
+// * generate JWT token
+
+// const token = generateToken(user._id.toString());
+
+res.status(201).json({
+    message: "Login successful",
+    success: true,
+    status: "Success",
+    data: user,
+});
+  } catch (error) {
+    next(error);
+  }
+};
 
 
 // * change password
